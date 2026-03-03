27b2321c99ce8ec7965d37697d3b81ea2d17a969
Aizek <pavelgemer1256@gmail.com>
2026-03-03 23:05:50 +0300

    first commit

---
+++ calculator-service/src/test/java/com/example/calculator_service/CalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
